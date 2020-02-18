f9786380928ee1c440392245b2db433652a732f2
Javier Carriba <javier.carriba@telegraph.co.uk>
2020-02-18 17:26:44 +0000

    Improving testing

--- server/src/test/java/uk/co/telegraph/switchboard/SwitchboardApplicationTests.java
+++ server/src/test/java/uk/co/telegraph/switchboard/SwitchboardApplicationTests.java
@@ -16,17 +16,31 @@
 
 package uk.co.telegraph.switchboard;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @Tag("integration")
 class SwitchboardApplicationTests {
 
-  @Test
-  void contextLoads() {
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Test
+  void checkHealth() throws Exception {
+    mockMvc
+        .perform(get("/actuator/health"))
+        .andDo(print())
+        .andExpect(status().isOk());
   }
 
 }
