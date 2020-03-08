aa8853f66a66b8660e57ee0008514442b6b5f197
Xavier Carriba <javier.carriba@telegraph.co.uk>
2020-03-08 04:03:47 +0000

    Application controller scaffolding

--- server/src/main/java/uk/co/telegraph/switchboard/application/ApplicationController.java
+++ server/src/main/java/uk/co/telegraph/switchboard/application/ApplicationController.java
@@ -16,15 +16,75 @@
 
 package uk.co.telegraph.switchboard.application;
 
+import java.util.List;
+import javax.validation.Valid;
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import uk.co.telegraph.switchboard.application.dto.ApplicationDto;
+import uk.co.telegraph.switchboard.application.dto.ApplicationRequest;
+import uk.co.telegraph.switchboard.application.mappers.ApplicationMapper;
+import uk.co.telegraph.switchboard.domain.Application;
+import uk.co.telegraph.switchboard.factories.ApplicationFactory;
 import uk.co.telegraph.switchboard.repositories.ApplicationRepository;
 
 @RestController
+@RequestMapping("/api/application")
 public class ApplicationController {
 
+  private final ApplicationFactory applicationFactory;
   private final ApplicationRepository applicationRepository;
+  private final ApplicationMapper applicationMapper;
 
-  public ApplicationController(ApplicationRepository applicationRepository) {
+  public ApplicationController(
+      ApplicationFactory applicationFactory,
+      ApplicationRepository applicationRepository) {
+    this.applicationFactory = applicationFactory;
     this.applicationRepository = applicationRepository;
+    this.applicationMapper = Mappers.getMapper(ApplicationMapper.class);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ApplicationDto createApplication(
+      @RequestBody @Valid ApplicationRequest request) {
+    Application application = applicationFactory.createApplication(
+        request.getName(),
+        request.getDescription()
+    );
+
+    applicationRepository.saveApplication(application);
+
+    return applicationMapper.domainToDto(application);
+  }
+
+  @GetMapping
+  public List<ApplicationDto> getApplicationList() {
+    throw new UnsupportedOperationException();
+  }
+
+  @GetMapping("/{id}")
+  public ApplicationDto findApplicationById(@PathVariable String id) {
+    throw new UnsupportedOperationException();
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteApplication(@PathVariable String id) {
+    throw new UnsupportedOperationException();
+  }
+
+  @PutMapping("/{id}")
+  public void updateApplication(
+      @PathVariable String id,
+      @RequestBody ApplicationRequest request) {
+    throw new UnsupportedOperationException();
   }
 }
---
+++ server/src/test/java/uk/co/telegraph/switchboard/application/dto/ApplicationRequestTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.telegraph.switchboard.application.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ApplicationRequestTest {
+
+  private static final String APPLICATION_NAME = "Website";
+  private static final String APPLICATION_DESCRIPTION = "Public website";
+
+  private static final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+
+  private Validator validator;
+  private ApplicationRequest applicationRequest;
+
+  @BeforeEach
+  void setUp() {
+    validator = validatorFactory.getValidator();
+    applicationRequest = new ApplicationRequest();
+  }
+
+  @Test
+  void should_set_name_and_description_on_constructor() {
+    applicationRequest = new ApplicationRequest(APPLICATION_NAME, APPLICATION_DESCRIPTION);
+
+    assertThat(applicationRequest)
+        .hasFieldOrPropertyWithValue("name", APPLICATION_NAME)
+        .hasFieldOrPropertyWithValue("description", APPLICATION_DESCRIPTION);
+  }
+
+  @Test
+  void should_set_and_return_name() {
+    applicationRequest.setName(APPLICATION_NAME);
+
+    assertThat(applicationRequest.getName())
+        .isEqualTo(APPLICATION_NAME);
+  }
+
+  @Test
+  void should_set_and_return_description() {
+    applicationRequest.setDescription(APPLICATION_DESCRIPTION);
+
+    assertThat(applicationRequest.getDescription())
+        .isEqualTo(APPLICATION_DESCRIPTION);
+  }
+
+  @Test
+  void should_validate() {
+    applicationRequest.setName(APPLICATION_NAME);
+
+    Set<ConstraintViolation<ApplicationRequest>> violations
+        = validator.validate(applicationRequest);
+
+    assertThat(violations)
+        .isEmpty();
+  }
+
+  @Test
+  void should_not_validate_if_name_is_null() {
+    Set<ConstraintViolation<ApplicationRequest>> violations
+        = validator.validate(applicationRequest);
+
+    assertThat(violations)
+        .hasSize(1);
+    ConstraintViolation<ApplicationRequest> violation = violations.iterator().next();
+    assertThat(violation.getPropertyPath().toString())
+        .isEqualTo("name");
+  }
+
+  @Test
+  void should_not_validate_if_name_is_empty() {
+    applicationRequest.setName("   ");
+
+    Set<ConstraintViolation<ApplicationRequest>> violations
+        = validator.validate(applicationRequest);
+
+    assertThat(violations)
+        .hasSize(1);
+    ConstraintViolation<ApplicationRequest> violation = violations.iterator().next();
+    assertThat(violation.getPropertyPath().toString())
+        .isEqualTo("name");
+  }
+
+  @Test
+  void should_return_string_representation() {
+    assertThat(applicationRequest.toString())
+        .startsWith("ApplicationRequest");
+  }
+}
